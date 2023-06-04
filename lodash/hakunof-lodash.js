e9b2d370c8fbbda7eb4f03d9cbca418fc66b95e4
hakuno <hakuno@gmail.com>
2023-06-04 21:54:14 +0800

    0604L3

--- lodash/hakunof-lodash.js
+++ lodash/hakunof-lodash.js
@@ -59,7 +59,78 @@ var hakunof = {
     }
   },
 
-  
+  findLastIndex: function(array, f, fromIndex = array.length - 1) {
+    for (var i = fromIndex; i >= 0; i--) {
+      if (typeof(f) == 'function') {
+          if (f(array[i])) {
+            return i
+        }
+      }
+
+      if (typeof(f) == 'object') {
+        if (Array.isArray(f)) {
+          if (f[0] in array[i] && f[1] == array[i][f[0]]) {
+            return i
+          }
+        } else {
+          var res = true
+          for (var key in f) {
+            if (f[key] != array[i][key]) {
+              res = false
+              break
+            }
+          }
+          if (res) {
+            return i
+          }
+        }
+
+        if (typeof(f) == 'string') {
+          if (array[i][f]) {
+            return i
+          }
+        }
+      }
+
+      return -1
+    }
+
+  },
+
+  flatten: function(array) {
+    for (var i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+        array.splice(i, 1, ...array[i])
+        i++
+      }
+    }
+    return array
+  },
+
+  flattenDeep: function(array) {
+    for (var i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+        array.splice(i, 1, ...array[i])
+      }
+    }
+    return array
+  },
+
+  flattenDepth: function(array, depth = 1) {
+    var count = 1
+    while (count <= depth) {
+      for (var i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+          array.splice(i, 1, ...array[i])
+          i++
+        }
+      }
+      count++
+    }
+    return array
+  },
+
+
 
 }
 
